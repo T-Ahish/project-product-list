d8c533b042df602fe215c1b5c8bf30ccc5a782e7
T-Ahish <ahishgameking@gmail.com>
2026-01-11 19:02:21 +0530

    Add Product List Layout with style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import Header from "./components/header/Header";
 import "./App.css";
+import ProductList from "./components/productList/ProductList";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <ProductList />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/productList/ProductList.tsx
@@ -0,0 +1,24 @@
+import styles from "./ProductList.module.css";
+
+const ProductList = () => {
+  return (
+    <article className={styles.productList}>
+      <h1 className={styles.productListHeading}>Add Products</h1>
+      <section className={styles.listLabels}>
+        <h2>Product</h2>
+        <h2>Discount</h2>
+      </section>
+      <section>
+        <p>1.</p>
+        <section>
+          <input type="text" placeholder="Select Product" readOnly />
+          <button>Edit</button>
+        </section>
+        <button>Add Discount</button>
+      </section>
+      <button>Add Product</button>
+    </article>
+  );
+};
+
+export default ProductList;
